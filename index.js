e476b3287270daf18b39af9aa0f34ab0bf125cc3
AdamJablonka <ajjablonka@gmail.com>
2022-06-22 20:35:00 -0400

    spelling

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 /**
 * This project uses NodeJS to create a synchronous callback of information from one API to another.
 * The two API's are Abstract API (https://www.abstractapi.com/ip-geolocation-api) and the Free Weather API from Open-Meteo (open-meteo.com/en).
-* NOTE: You will need to prove your own API key for the abstract api. You can place the API key in the "credentials.json" file located in the "auth"
+* NOTE: You will need to prove your own API key for the Abstract API. You can place the API key in the "credentials.json" file located in the "auth" directory.
 */
 const fs = require('fs');
 const http = require('http');
